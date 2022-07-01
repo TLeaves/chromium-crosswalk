5242fe43b8073e8ae0dd2a15e1ff2641edc30523
TLeaves <664570530@qq.com>
2022-07-01 20:25:40 +0800

    [Feature] clean all files of old version.

--- demo/b.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-// extern void foo_common();
-
-void foo_B(const std::string& text) {
-    // foo_common();
-    std::cout << "B: " << text << std::endl;
-}
